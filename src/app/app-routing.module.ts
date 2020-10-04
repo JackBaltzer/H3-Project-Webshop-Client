a6515432b25c2ac8e569693137c3014fcba4de5b
Jack Baltzer <baltzer@outlook.com>
2020-10-04 21:16:57 +0200

    jwt and route guard implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './_helpers';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { CategoriesComponent } from './admin/categories';
+import { LoginsComponent } from './admin/logins';
+import { Role } from './_models';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'admin', children: [
+            {
+                path: 'categories',
+                component: CategoriesComponent,
+                canActivate: [AuthGuard],
+                data: {
+                    roles: [Role.Admin]
+                }
+            },
+            {
+                path: 'logins',
+                component: LoginsComponent,
+                canActivate: [AuthGuard],
+                data: {
+                    roles: [Role.Admin]
+                }
+            }
+        ]
+    },
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
